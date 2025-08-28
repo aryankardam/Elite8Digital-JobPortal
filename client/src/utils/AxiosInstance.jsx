a42ca746bee4c82a03c9d1671089bb3065c3d143
aryankardam <145225226+aryankardam@users.noreply.github.com>
2025-08-28 11:20:05 +0530

    Set Axios timeout to 10 seconds
    Increased timeout for Axios instance to 10 seconds.

--- client/src/utils/AxiosInstance.jsx
+++ client/src/utils/AxiosInstance.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://elite8digital-jobportal.onrender.com', // Your backend server
+  baseURL: 'https://elite8digital-jobportal.onrender.com',
+  timeout: 10000,
   withCredentials: true, // Optional: if using cookies or tokens
 });
 
